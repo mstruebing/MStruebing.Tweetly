65ea4ccaa37001e960c94f9fa794cdd1df6b9b83
Max Strübing <max_struebing@gmx.de>
2016-01-07 10:25:59 +0100

    [TASK] Add Brics as frontend framework and gulp configuration

---
+++ Gulpfile.babel.js
@@ -0,0 +1,24 @@
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import minifycss from 'gulp-cssnano';
+import minifyjs from 'gulp-minify';
+
+let basePath = './Packages/Application/MStruebing.Tweetly';
+
+let paths = {
+    'private': basePath + '/Resources/Private/',
+    'public': basePath + '/Resources/Public/'
+};
+
+gulp.task('MStruebing.Tweetly:compile:styles', () => {
+  return gulp.src(paths.private + 'Styles/Main.scss')
+    .pipe(sass())
+    .pipe(minifycss())
+    .pipe(gulp.dest(paths.public + 'Styles/'))
+});
+
+gulp.task('MStruebing.Tweetly:compile:scripts', () => {
+  return gulp.src(paths.private + 'JavaScripts/App.js')
+    .pipe(minifyjs())
+    .pipe(gulp.dest(paths.public + 'JavaScripts/'))
+});
---
+++ Resources/Private/JavaScripts/App.js
@@ -0,0 +1,2 @@
+const hey = 'abc';
+console.log(hey);
